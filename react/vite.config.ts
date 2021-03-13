266304f3a3ea91361b3c7818a1df885b06bd24bd
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-03-13 14:06:50 +0900

    :wrench: (react) Use windi CSS

--- react/vite.config.ts
+++ react/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
 import { resolve } from 'path'
+import WindiCSS from 'vite-plugin-windicss'
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
@@ -11,5 +12,5 @@ export default defineConfig({
   esbuild: {
     jsxInject: `import React from 'react'`
   },
-  plugins: [reactRefresh()]
+  plugins: [reactRefresh(), WindiCSS()]
 })
